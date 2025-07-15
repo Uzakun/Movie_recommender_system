29c21817645d4dc8966c94466b225b95c044bb35
Shwetank Chandra <chandreium@gmail.com>
2025-07-15 16:02:57 +0530

    Make easy to use the webapp

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer 
 from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
 import requests
